b2648f647817f8adc29c4637cbee34de935da9a3
LuismallitoDev <cardonaluisma@gmail.com>
2024-11-25 23:09:41 -0500

    Entrega

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -3,26 +3,24 @@ package classroom;
 public class Asignatura {
 
     public String nombre;
-    String n = nombre;
-    public int codigoInterno;
-    public int codigoExterno;
+    public double codigoInterno;
+    public int codigoExterno; 
     public Tipo tipo;
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
+    public Asignatura(Tipo tipo,int codigoInterno) {
+        this("Sin nombre", codigoInterno, 0,tipo); 
     }
 
-    public Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
+    public Asignatura(int codigoExterno, Tipo tipo) {
+        this("Sin nombre", 0, codigoExterno,tipo); 
     }
 
     public Asignatura(String nombre, Tipo tipo) {
-        this(nombre, 0, 0,tipo);
+        this(nombre, 0, 0,tipo); 
     }
 
     public Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
@@ -39,8 +37,8 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
+    public void cambiarDatos(double codigoInterno) {
+        this.codigoInterno = (int) codigoInterno; //Casting explicito de un double a un int.
     }
 
     public void cambiarDatos(int codigoExterno) {
@@ -56,6 +54,6 @@ public class Asignatura {
     }
     
     public void setCodigoInterno(double codigoInterno) {
-        this.codigoInterno = (int) codigoInterno;
+        this.codigoInterno = (int) codigoInterno; //Casting explicito de un double a un int.
     }
 }
--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,23 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+	DISCIPLINAR(10, "Disciplinar"),
+	FUNDAMENTACION(20, "Fundamentación"),
+	ELECTIVA(30, "Electiva");
 	
 	private int codigo;
 	private String nombre;
-	public Tipo(int codigo,String nombre) {
+
+	private Tipo(int codigo,String nombre) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 	}
+
+	//Getters para poder acceder a los valores de instancia y no crearlos por que sí
+    public int getCodigo() {
+        return codigo;
+    }
+	public String getNombre() {
+        return nombre;
+    }
 }
